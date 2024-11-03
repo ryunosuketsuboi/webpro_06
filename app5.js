5c4b3487ce06d0c17fc0fa342627a7bb42dc9fbf
Tsuboi Ryuunosuke <trad1270@au.com>
2024-11-03 14:22:04 +0900

    app5.jsにじゃんけんの勝ち負け判定を追加

--- app5.js
+++ app5.js
@@ -28,28 +28,41 @@ app.get("/luck", (req, res) => {
 });
 
 app.get("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  let hand = req.query.hand; // プレイヤーの手
+  let win = Number(req.query.win); // 勝利数
+  let total = Number(req.query.total); // 試合数
+
+  const num = Math.floor(Math.random() * 3 + 1); // CPUの手をランダムに決定
   let cpu = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
+  if (num === 1) cpu = 'グー';
+  else if (num === 2) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
-  total += 1;
+
+  // 勝敗の判定
+  let judgement = '';
+  if (hand === cpu) {
+    judgement = '引き分け';
+  } else if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1; // 勝った場合は勝利数を増やす
+  } else {
+    judgement = '負け';
+  }
+  total += 1; // 試合数を増やす
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
-    total: total
-  }
-  res.render( 'janken', display );
+    total: total,
+  };
+  
+  res.render('janken', display);
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
